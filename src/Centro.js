9c0c9406451c6e0faf3ee34cab3364f50590bfeb
Franklin Rodriguez <frank14082013@gmail.com>
2022-11-20 10:31:11 -0600

    segmentos arriba, centro y abajo

---
+++ src/Centro.js
@@ -0,0 +1,25 @@
+
+import './Centro.css';
+/*
+import logo from './logo.svg';
+*/
+
+export default function Centro(props) {
+return (
+<div className="Centro">{/* root-Centro */}
+
+
+<div className="page-single movie_list">
+<div className="container">
+<div className="row ipad-width2">
+<div className="col-md-8 col-sm-12 col-xs-12">
+{props.children}
+</div>
+</div>
+</div>
+</div>
+
+{/* root-Centro */}</div>);
+}
+/* export default Centro; */
+
--- src/Peliculasite.js
+++ src/Peliculasite.js
@@ -3,6 +3,7 @@ import './Peliculasite.css';
 import Listapelicula from './componentes/Listapelicula/Listapelicula';
 import Arriba from './Arriba';
 import Abajo from './Abajo';
+import Centro from './Centro';
 /*
 import logo from './logo.svg';
 */
@@ -14,19 +15,14 @@ return (
 
 <Arriba />
 
-<div className="page-single movie_list">
-<div className="container">
-<div className="row ipad-width2">
-<div className="col-md-8 col-sm-12 col-xs-12">
-
-<br /><br />
+<Centro>
 
 <Listapelicula titulo="RESIDENT EVIL"      descripcion="pelicula basada en hechos reales, los nombres se cambiaron para proteger su identidad real" duracion="1:59:18" estrellas="9" clasificacion="+18" fechaestreno="2002-02-09" director="Rodriguez Franklin" protagonista="Franklin Rodriguez" caratula="https://i.blogs.es/c82453/cartel-resident-evil-el-capitulo-final/1366_2000.jpg"                                />
 <Listapelicula titulo="X-MEN"              descripcion="pelicula basada en hechos reales, los nombres se cambiaron para proteger su identidad real" duracion="1:59:18" estrellas="9" clasificacion="+18" fechaestreno="2002-02-09" director="Rodriguez Franklin" protagonista="Franklin Rodriguez" caratula="https://super-ficcion.com/wp-content/uploads/2022/02/Como-ver-en-orden-las-peliculas-de-los-X-Men-780x470.jpg" />
 <Listapelicula titulo="DESTINO FINAL"      descripcion="pelicula basada en hechos reales, los nombres se cambiaron para proteger su identidad real" duracion="1:59:18" estrellas="9" clasificacion="+18" fechaestreno="2002-02-09" director="Rodriguez Franklin" protagonista="Franklin Rodriguez" caratula="https://www.themoviedb.org/t/p/w500/yrQjtYmEsnc1hlDlXQZ29EMSFbp.jpg"                                           />
 <Listapelicula titulo="RAPIDOS Y FURIOSOS" descripcion="pelicula basada en hechos reales, los nombres se cambiaron para proteger su identidad real" duracion="1:59:18" estrellas="9" clasificacion="+18" fechaestreno="2002-02-09" director="Rodriguez Franklin" protagonista="Franklin Rodriguez" caratula="https://static.cinepolis.com/resources/mx/movies/posters/414x603/34669-453300-20210622125843.jpg"              />
 
-</div></div></div></div>
+</Centro>
 
 <Abajo />
 
