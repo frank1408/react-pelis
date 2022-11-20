9a001c56a76932865887a399fba113d72e519abd
Franklin Rodriguez <frank14082013@gmail.com>
2022-11-20 08:50:45 -0600

    componente Listapelicula

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import Peliculasite from './Peliculasite';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Peliculasite />
+  </React.StrictMode>
+);
+reportWebVitals();
