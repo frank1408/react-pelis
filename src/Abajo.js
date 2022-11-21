2fe2dd208c231a917f56da179566e01777c8c455
Franklin Rodriguez <frank14082013@gmail.com>
2022-11-21 10:20:17 -0600

    footer

--- src/Abajo.js
+++ src/Abajo.js
@@ -1,64 +1,29 @@
 
 import './Abajo.css';
-/*
-import logo from './logo.svg';
-*/
 
 export default function Abajo() {
 return (
 <div className="Abajo">{/* root-Abajo */}
 
-<footer className="ht-footer">
-<div className="container">
-<div className="flex-parent-ft">
-<div className="flex-child-ft item1">
+<div>
+<p>Direccion</p>
 <a href="https://google.com">
- <img className="logo" src="images/logo.png" alt="logo-empresarial" width="108px" />
+18 Avenida 5-25 Zona 10 NOMBRE-DE-COLONIA<br />Guatemala, Guatemala
 </a>
-
-<p>
-<a href="https://google.com">
-18 Avenida, 5-25 Zona 1 NombreDeColonia<br />
-Guatemala, Guatemala
-</a>
-</p>
-
-<p>
-<a href="https://google.com">
-Llamame:
-(+502) 3975 7208
-</a>
-</p>
-
 </div>
 
-
-<div className="flex-child-ft item2">
-<h4>Recursos</h4>
-<ul>
-<li><a href="https://google.com">Sobre nosotros</a></li> 
-<li><a href="https://google.com">Contactanos</a></li>
-<li><a href="https://google.com">Foros</a></li>
-<li><a href="https://google.com">Blog</a></li>
-<li><a href="https://google.com">Centro de ayuda</a></li>
-</ul>
+<div>
+<p>Contactenos</p>
+<a href="tel:+50212341234">Via telefonica</a>
+<a href="mailto:alguien@gmail.com">Via correo electronico</a>
 </div>
 
-
-
-<div className="flex-child-ft item3">
-<h4>Legal</h4>
-<ul>
-<li><a href="https://google.com">Terminos de uso</a></li> 
-<li><a href="https://google.com">Politica de privacidad</a></li>	
-<li><a href="https://google.com">Seguridad</a></li>
-</ul>
+<div>
+<p>Legal</p>
+<a href="https://google.com">Terminos de uso</a>
+<a href="https://google.com">Politica de privacidad</a>
 </div>
-</div>
-</div>
-</footer>
 
 {/* root-Abajo */}</div>);
 }
-/* export default Abajo; */
 
--- src/arriba.js
+++ src/arriba.js
@@ -1,62 +1,16 @@
 
 import './Arriba.css';
-/*
-import logo from './logo.svg';
-*/
 
 export default function Arriba() {
 return (
 <div className="Arriba">{/* root-Arriba */}
 
-<header className="ht-header">
-	<div className="container">
-	<nav className="navbar navbar-default navbar-custom">
-	
-	<div className="navbar-header logo">
-	<a href="https://google.com"><img className="logo" src="images/logo.png" alt="logo-empresarial" width="108" /></a>
-	</div>
-	
-	
-	<div className="collapse navbar-collapse flex-parent" id="bs-example-navbar-collapse-1">
-	<ul className="nav navbar-nav flex-child-menu menu-left">
-	
-	
-	<li className="dropdown first">
-		<a href="https://google.com"
-		className="btn btn-default dropdown-toggle lv1"
-		data-toggle="dropdown"
-		data-hover="dropdown">Peliculas</a>
-	{/*
-	<ul className="dropdown-menu level1">
-	<li className="dropdown">
-	<a href="https://google.com" className="dropdown-toggle" data-toggle="dropdown" >Movie grid<i className="ion-ios-arrow-forward"></i></a>
-	<ul className="dropdown-menu level2">
-	<li><a href="moviegrid.html">Movie grid</a></li>
-	<li><a href="moviegridfw.html">movie grid full width</a></li>
-	</ul>
-	</li>			
-	<li><a href="movielist.html">Movie list</a></li>
-	<li><a href="moviesingle.html">Movie single</a></li>
-	<li className="it-last"><a href="seriessingle.html">Series single</a></li>
-	</ul>
-	*/}
-	</li>
-	
-	
-	
-	<li className="dropdown first">
-		<a href="https://google.com"
-		className="btn btn-default dropdown-toggle lv1"
-		data-toggle="dropdown"
-		data-hover="dropdown">Series</a>
-	</li>
-	</ul>
-	</div>
-	</nav>
-	</div>
-</header>
+<a href="https://google.com">
+<img src="images/logo.png" alt="logo" />
+</a>
+<a href="https://google.com">Peliculas</a>
+<a href="https://google.com">Series</a>
 
 {/* root-Arriba */}</div>);
 }
 /* export default Arriba; */
-
